30a6835f782ecb7c6bf08d0e7e72966af4d2d8f4
db <dengbin@meishubao.com>
2026-08-14 16:19:20 +0800

    init

---
+++ SimpleMusic/SimpleMusic/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SimpleMusic
+//
+//  Created by db on 2026/8/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SimpleMusic/SimpleMusic/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  SimpleMusic
+//
+//  Created by db on 2026/8/14.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
